a081df791e3517931a86822dcca5db7b231a8fa4
Francois Charbonneau <fcharbonneau@gosecure.ca>
2017-03-14 16:24:08 -0400

    Added some stuff to test features with socket.io

--- ceopardy.py
+++ ceopardy.py
@@ -16,9 +16,13 @@
 # GNU General Public License for more details.
 #
 from flask import Flask, render_template
+from flask_socketio import SocketIO, send, emit
 from ceopardy.controller import Controller
+import sys
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Alex Trebek forever!'
+socketio = SocketIO(app)
 
 @app.context_processor
 def inject_config():
@@ -33,6 +37,22 @@ def start():
 def gameboard():
     return render_template("gameboard.html")
 
+@app.route('/host')
+def host():
+    return render_template('host.html')
+
+@app.route('/player')
+def player():
+    return render_template('player.html')
+
+@app.route('/viewer')
+def viewer():
+    return render_template('viewer.html')
+
+@socketio.on('click')
+def handle_message(data):
+    print('received data: ' + data["button"], file=sys.stderr)
+    emit("change", data, broadcast=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
